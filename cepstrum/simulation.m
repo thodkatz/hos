8e764f1901c85fff182accddc53dbb4cb93a14ce
thodkatz <thodkatz@gmail.com>
2021-05-28 19:28:57 +0300

    Minor changes

--- cepstrum/simulation.m
+++ cepstrum/simulation.m
@@ -13,55 +13,55 @@ sound = sound(:,1); % i don't know why I have two columns
 
 estimatePitch = 0.005;
 
-% get time interval 2sec - 2.002 sec
+% start from 2sec for the frame
 % assuming voice lasts over 2 seconds
 Ts = 1/fs;
 t = 0:Ts:(N-1)*Ts;
-periods = 20;
+periods = 4;
 tStart   = round(2/(Ts));
 tEnd     = round((2 + periods*estimatePitch)/(Ts));
-tEndHalf = round((2 + periods*estimatePitch/2)/(Ts));
 soundWindow = sound(tStart:tEnd);
+range     = tStart:tEnd;
 N = numel(soundWindow);
 
 figure
 subplot(2, 2, 1)
-plot(t(tStart:tEnd)*1000,soundWindow);
+plot(t(1:numel(range))*1000,soundWindow);
 xlabel('Time (ms)'),axis tight
 title('Time signal')
 
 % hamming window
 subplot(2, 2, 3)
 soundWindow = soundWindow .* hamming(N);
-plot(t(tStart:tEnd),soundWindow)
-title('Windowed signal')
-axis tight
+plot(t(1:numel(range))*1000,soundWindow)
+xlabel('Time (ms)'),title('Hamilton window applied'),axis tight
 
+% ceps and reps in full signal
+halfCeps = round(numel(range)/2);
 subplot(2, 2, 2)
-c = cceps(soundWindow);
-range = tStart:tEndHalf;
-plot(t(range)*1000,c(1:numel(range)))
-xlabel('Time (ms)'),axis tight
-title('Complex cepstrum')
+rcep = rceps(sound);
+plot(t(1:halfCeps)*1000,rcep(1:halfCeps))
+xlabel('quefrency (ms)'),title('Real cepstrum')
 
 subplot(2, 2, 4)
-r = rceps(soundWindow);
-plot(t(range)*1000,r(1:numel(range)))
-xlabel('Time (ms)'),axis tight
-title('Real cepstrum')
+rcep = cceps(sound);
+plot(t(1:halfCeps)*1000,rcep(1:halfCeps))
+xlabel('quefrency (ms)'),title('Complex cepstrum')
 
 figure
 Y = fftshift(fft(soundWindow));
 fshift = (-N/2:N/2-1)*(fs/N);
 plot(fshift,abs(Y)/N)
+xlabel('frequency (Hz)'),title('FFT')
 
+% ceps and reps in frame 
 figure
-rcep = rceps(sound);
-plot(t(1:numel(range)),rcep(1:numel(range)))
-title('Real cepstrum all signal Hamming window')
+subplot(2, 1, 1)
+rcep = rceps(soundWindow);
+plot(t(1:halfCeps)*1000,rcep(1:halfCeps))
+xlabel('quefrency (ms)'),title('Real cepstrum')
 
-figure
-plot(rceps(sound))
-
-figure
-plot(cceps(sound))
+subplot(2, 1, 2)
+rcep = cceps(soundWindow);
+plot(t(1:halfCeps)*1000,rcep(1:halfCeps))
+xlabel('quefrency (ms)'),title('Complex cepstrum')
